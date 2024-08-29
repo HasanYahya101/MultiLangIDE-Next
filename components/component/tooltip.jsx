e61f4bc22c34a5503be2747b0f583e1c376304ce
Hasan Yahya <hasna786yahya@gmail.com>
2024-08-29 12:40:54 +0500

    Initialized Button Tooltip

---
+++ components/component/tooltip.jsx
@@ -0,0 +1,12 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export function ButtonToolTip() {
+    return (
+        <div></div>
+    );
+}
